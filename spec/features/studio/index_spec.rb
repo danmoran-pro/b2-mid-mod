364d3e0328c824923d9defe53efdd1ef49399ec6
Daniel <danmoran.pro@gmail.com>
2020-02-14 10:28:39 -0700

    user story 1 complete

--- spec/features/studio/index_spec.rb
+++ spec/features/studio/index_spec.rb
@@ -7,39 +7,41 @@ RSpec.describe "As a User" do
         @studio_2 = Studio.create!(name: "Universal Pictures")
         @studio_3 = Studio.create!(name: "Lionsgate Films")
 
-        @movie_1 = @studio_1.create!(name: "Toy Story" , creation_year: 1995, genre: "Adventure" )
-        # @movie_2 = @studio_2.create!(name: "Despicable Me" , creation_year: 2010, genre: "Family" )
-        # @movie_3 = @studio_3.create!(name: "The Hunger Games" , creation_year: 2012, genre: "Thriller" )
+        @movie_1 = @studio_1.movies.create!(name: "Toy Story" , creation_year: 1995, genre: "Adventure" )
+        @movie_2 = @studio_2.movies.create!(name: "Despicable Me" , creation_year: 2010, genre: "Family" )
+        @movie_3 = @studio_3.movies.create!(name: "The Hunger Games" , creation_year: 2012, genre: "Thriller" )
     end 
     it "I see a list of all of the movie studios " do
         visit studios_path
         
-        expect(page).to have_contents("Pixar")
+        expect(page).to have_content("Pixar")
 
-        expect(page).to have_contents("Universal Pictures")
+        expect(page).to have_content("Universal Pictures")
 
-        expect(page).to have_contents("Lionsgate Films")
+        expect(page).to have_content("Lionsgate Films")
     end
 
-    # it "underneath each studio, I see the names of all of its movies. " do 
+   it "underneath each studio, I see the names of all of its movies. " do
+      visit studios_path
 
-    #   within("#studio-#{@studio_1.id}") do
-    #     expect(page).to have_content("Toy Story")
-    #   end
+      within "#studio-#{@studio_1.id}" do
+        within "#movies" do
+          expect(page).to have_content("Toy Story")
+      end
+    end
 
-    #   within("#studio-#{@studio_2.id}") do
-    #     expect(page).to have_content("Despicable Me")
-    #   end
+      within "#studio-#{@studio_2.id}" do
+        within "#movies" do
+          expect(page).to have_content("Despicable Me")
+        end
+      end
 
-    #   within("#studio-#{@studio_3.id}") do
-    #     expect(page).to have_content("The Hunger Games")
-    #   end
-    # end 
+      within("#studio-#{@studio_3.id}") do
+        within "#movies" do
+          expect(page).to have_content("The Hunger Games")
+        end 
+      end
+    end 
   end
 end
 
-
-# As a user, 
-# When I visit the studio index page
-# I see a list of all of the movie studios 
-# And underneath each studio, I see the names of all of its movies.
